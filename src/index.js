2490d270de91f376477cfc18fdd7db0013e2afee
Avathanshu <avathanshu@gmail.com>
2024-06-29 23:08:58 +1200

    dropdown template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-import './style.css';
+import "./style.css";
 
 // Code below this line
 
+const btnDropdown = document.querySelector(".dropdown>button");
+const listDropdown = document.querySelector(".dropdown>ul");
+
+let isClicked = false;
+function listVisibility(list, clickConditon) {
+  if (clickConditon) {
+    list.style.display = "block";
+  } else if (clickConditon === false) {
+    list.style.display = "none";
+  }
+}
+
+btnDropdown.addEventListener("click", () => {
+  if (isClicked === false) {
+    isClicked = true;
+    listVisibility(listDropdown, isClicked);
+  } else if (isClicked === true) {
+    isClicked = false;
+    listVisibility(listDropdown, isClicked);
+  }
+});
+
+listVisibility(listDropdown, isClicked);
